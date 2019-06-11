547fc01937f45f7add5083685fb6c784411c7d0c
SaiSindhuja799 <51689337+SaiSindhuja799@users.noreply.github.com>
2019-06-11 10:53:05 +0530

    Update and rename cap1 to cap1.c

--- cap1.c
+++ cap1.c
@@ -1,3 +1,4 @@
+
 #include<stdio.h>
 int main()
 {
@@ -5,12 +6,16 @@ int main()
 	int len,ctr;
 	scanf("%[^\n]s",s);
 	for(len=0;s[len];len++);
+	if(isalpha(s[ctr]))
 	s[0]=s[0]-32;
 	for(ctr=0;ctr<len;ctr++)
 	{
+		if(isalpha(s[ctr]))
+		{
 		if(s[ctr]==' ')
 		{
 			s[ctr+1]=s[ctr+1]-32;
+		}
 		}
 	}
 	printf("%s",s);
